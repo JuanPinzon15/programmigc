29abb716778e93864b6474115cab76ec90d3d920
JuanPinzon15 <157433872+JuanPinzon15@users.noreply.github.com>
2025-02-19 17:35:49 -0600

    Create exercise8p123jp.c

---
+++ exercise8p123jp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int days_in_month, start_day;
+
+    // Get user input for number of days in the month and starting day of the week
+    printf("Enter number of days in month: ");
+    scanf("%d", &days_in_month);
+    printf("Enter starting day of the week (1=Sun, 7=Sat): ");
+    scanf("%d", &start_day);
+
+    // Print the calendar
+    for (int i = 1; i < start_day; i++) {
+        printf("   ");
+    }
+
+    // Print the days of the month
+    for (int day = 1; day <= days_in_month; day++) {
+        printf("%2d ", day); 
+
+        // Check if it's the last day of the week
+        if ((day + start_day - 1) % 7 == 0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
