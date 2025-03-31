487265150af2bc637814d4dc1fbc8f658c83923b
JuanPinzon15 <157433872+JuanPinzon15@users.noreply.github.com>
2025-03-31 14:13:31 -0500

    Create program#6ch9jp.c

---
+++ program#6ch9jp.c
@@ -0,0 +1,39 @@
+/*******************
+ * FILENAME: program#6ch9jp.c
+
+ * YOUR NAME: Juan Pinzon
+
+ * DESCRIPTION: This program asks the user to enter a value for x, calls the function to compute the value of the polynomial, and then displays the value returned by the function. Also there is a function that evaluates the polynomial.
+
+*********************/
+#include <stdio.h>
+
+// Recursive function to calculate x^n
+int power(int x, int n) {
+    if (n == 0) 
+        return 1;
+    else
+        return x * power(x, n - 1);
+}
+
+// Function to evaluate the polynomial
+int polynomial(int x) {
+    return (3 * power(x, 5)) + 
+           (2 * power(x, 4)) - 
+           (5 * power(x, 3)) - 
+           (power(x, 2)) + 
+           (7 * x) - 6;
+}
+
+int main() {
+    int x;
+    
+    // Get input from user
+    printf("Enter a value for x: ");
+    scanf("%d", &x);
+
+    // Compute and display the polynomial result
+    printf("The value of the polynomial is: %d\n", polynomial(x));
+
+    return 0;
+}
